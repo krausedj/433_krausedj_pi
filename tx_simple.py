73d1a2f6186c6c3bc6cedd105411fa6e4a2a7ad3
Daniel J. Krause <krausedj@gmail.com>
2020-08-16 21:51:08 +0100

    Fixes from deployed testing

--- tx_simple.py
+++ tx_simple.py
@@ -13,17 +13,25 @@ last_time = wave.points[0].time_us
 
 for point in wave.points:
     delta_time = point.time_us - last_time
+    last_time = point.time_us
     if point.state == 0:
         pulse_list.append(pigpio.pulse(0, 1<<gpio_pin, delta_time))
     else:
         pulse_list.append(pigpio.pulse(1<<gpio_pin, 0, delta_time))
 
 pi = pigpio.pi()
+pi.set_mode(gpio_pin, pigpio.OUTPUT)
+
 pi.wave_clear()
 pi.wave_add_generic(pulse_list)
 wave_pi = pi.wave_create()
+
 pi.wave_send_repeat(wave_pi)
 time.sleep(2)
-pi.wave_stop_tx()
+
+pi.wave_tx_stop()
 pi.wave_clear()
+
+pi.write(gpio_pin, 0)
+
 pi.stop()
